cce0199a5791f7a0ad4463bb649f11d7e60edc05
Gregório <30591064+gregorio1212@users.noreply.github.com>
2021-02-14 21:50:02 +0100

    Final version of main.lua

--- LUA_Nodemcu_files/main.lua
+++ LUA_Nodemcu_files/main.lua
@@ -1,31 +1,56 @@
 --[[
 main.lua
-by Patrick Lloyd
 
-This contains all the bits and pieces to connect to the broker, subscribe and
-and publish to topics, run various functions that interact with GPIO, and so
-on.
+File where MQTT interfacing and functions depending on payloads and topics are stored
 --]]
 
--- Holds dispatching keys to different topics. Serves as a makeshift callback
--- function dispatcher based on topic and message content
+-- Array that hold functions names and has as index the topics
 m_dis = {}
 
 
 -- Standard counter variable. Used for modulo arithmatic
 local count = 0
 
+function safety(m, pl)
+    m:publish("smarthome/info/alarm", "Roger that", 0, 0,
+           function(m) print("ALARM COMMAND") end)
+      
+    gpio.mode(PIN_BEEP, gpio.OUTPUT)
+    gpio.mode(PIN_ALARM, gpio.OUTPUT)
+    if pl == "beep" then
+        m:publish("smarthome/info/alarm", "beep", 0, 0,
+            function(m) print("RINGING ALARM") end)
+        gpio.write(PIN_ALARM, gpio.LOW)
+        gpio.write(PIN_BEEP, gpio.HIGH)
+    end
+
+    if pl == "off" then
+        m:publish("smarthome/info/alarm", "off", 0, 0,
+            function(m) print("ALARM TURNING OFF") end)
+        gpio.write(PIN_ALARM, gpio.LOW)
+        gpio.write(PIN_BEEP, gpio.LOW)
+    end
+        
+    if pl == "on" then
+        m:publish("smarthome/info/alarm", "on", 0, 0,
+            function(m) print("SETTING THE ALARM") end)
+        gpio.write(PIN_ALARM, gpio.HIGH)
+        gpio.write(PIN_BEEP, gpio.LOW)
+    end
+end
+       
+
 function animate(m, pl)
 	-- Confirm that an animation message was received on the /mcu/cmd topic
-	m:publish("/mcu/rgbled_status/", "--> ANIMATE COMMAND", 0, 0,
-			function(m) print("ANIMATE COMMAND") end)
+	m:publish("smarthome/info/ledstate", "Roger that", 0, 0,
+			function(m) print("LED COMMAND") end) --/mcu
 	
 	-- Main option control structure. Pretty gross-looking but it works
 	-- Option 0 turns everything off
 	if pl == "0" then
 		-- Confirm LED being turned off to serial terminal and MQTT broker
-		m:publish("/mcu/rgbled_status/", "--> LED OFF", 0, 0,
-			function(m) print("LED OFF") end)
+		m:publish("smarthome/info/ledstate", "0", 0, 0,
+			function(m) print("LED OFF") end) --/mcu
 		
 		-- Reset the counter and stop the timer from another function
 		count = 0
@@ -39,8 +64,8 @@ function animate(m, pl)
 	-- RBG Mode
 	elseif pl == "1" then
 		-- Confirm LED in RGB mode to serial terminal and MQTT broker
-		m:publish("/mcu/rgbled_status/", "--> R-G-B Mode", 0, 0,
-			function(m) print("RGB Mode") end)
+		m:publish("smarthome/info/ledstate", "1", 0, 0,
+			function(m) print("RGB Mode") end) --/mcu
 		
 		-- Just stop the timer from another loop
 		timer:stop()
@@ -55,8 +80,8 @@ function animate(m, pl)
 	-- Pick a "random" color and make it breathe mode
 	elseif pl == "2" then
 		-- Confirm LED in random breathe mode to serial terminal and MQTT broker
-		m:publish("/mcu/rgbled_status/", "--> Random-Breathe Mode", 0, 0,
-			function(m) print("Random-Breathe Mode") end)
+		m:publish("smarthome/info/ledstate", "2", 0, 0,
+			function(m) print("Random-Breathe Mode") end) --/mcu
 		
 		-- Reset the counter and stop the timer from another function
 		timer:stop()
@@ -66,11 +91,11 @@ function animate(m, pl)
 		-- full brightness (percent = 100)
 		local percent = 100
 		local count_up  = false
-		local red = (tmr.now()*tmr.now()) % 512
-		tmr.delay(red)
-		local grn = (tmr.now()*tmr.now()) % 512
-		tmr.delay(grn)
-		local blu = (tmr.now()*tmr.now()) % 512
+		local red = (tmr.now()) % 512
+		--tmr.delay(red)
+		local grn = (tmr.now()) % 512
+		--tmr.delay(grn)
+		local blu = (tmr.now()) % 512
 
 		-- Breather alarm function run every 20 ms
 		timer:alarm(20,tmr.ALARM_AUTO,function()
@@ -98,8 +123,8 @@ function animate(m, pl)
 	-- Lots of random blinking craziness
 	elseif pl == "3" then
 		-- Confirm LED in disco mode to serial terminal and MQTT broker
-		m:publish("/mcu/rgbled_status/", "--> Disco Mode", 0, 0,
-			function(m) print("Disco Mode") end)
+		m:publish("smarthome/info/ledstate", "3", 0, 0,
+			function(m) print("Disco Mode") end) --/mcu
 		
 		-- Reset the counter and stop the timer from another function
 		timer:stop()
@@ -107,33 +132,35 @@ function animate(m, pl)
 		
 		-- Crazy disco alarm every 20 ms
 		timer:alarm(20,tmr.ALARM_AUTO,function()
-			pwm.setduty(PIN_GRN, (tmr.now()*tmr.now())%512)
-			pwm.setduty(PIN_RED, (tmr.now()*tmr.now())%512)
-			pwm.setduty(PIN_BLU, (tmr.now()*tmr.now())%512)
+			pwm.setduty(PIN_GRN, (tmr.now())%512)
+			pwm.setduty(PIN_RED, (tmr.now())%512)
+			pwm.setduty(PIN_BLU, (tmr.now())%512)
 		end)
 	
 	-- Something went wrong somehow
 	else
 		-- Print and publish to serial terminal and MQTT broker respectively
 		-- that something went wrong
-		m:publish("/mcu/rgbled_status/", "--> Error: Unknown Command", 0, 0,
-			function(m) print("ERROR: UNKNOWN COMMAND") end)
+		m:publish("smarthome/info/ledstate", "--> Error: Unknown Command", 0, 0,
+			function(m) print("ERROR: UNKNOWN COMMAND") end) --/mcu
 	end
 end
 
 
 -- As part of the dispatcher algorithm, this assigns a topic name as a key or
 -- index to a particular function name
-m_dis["/mcu/cmd/animate"] = animate
+m_dis["smarthome/cmd/led"] = animate -- 0 1 2 3
+m_dis["smarthome/cmd/alarm"] = safety -- beep on off
 
 -- initialize mqtt client with keepalive timer of 60sec
+-- we could set clean session to 0 to have a "persisten connection"
 m = mqtt.Client(MQTT_CLIENTID, 60, "brazil", "inpoland2021")
 
 
 -- Set up Last Will and Testament (optional)
 -- Broker will publish a message with qos = 0, retain = 0, data = "offline"
 -- to topic "/lwt" if client don't send keepalive packet
-m:lwt("/lwt", "Oh noes! Plz! I don't wanna die!", 0, 0)
+m:lwt("smarthome/info/device1", "offline", 0, 1)
 
 
 -- When client connects, print status message and subscribe to cmd topic
@@ -142,10 +169,15 @@ m:on("connect", function(m)
 	print ("\n\n", MQTT_CLIENTID, " connected to MQTT host ", MQTT_HOST,
 		" on port ", MQTT_PORT, "\n\n")
 
+    m:publish("smarthome/info/device1", "online", 0, 0,
+            function(m) print("Device 1 is online") end)
+
 	-- Subscribe to the topic where the ESP8266 will get commands from
-	m:subscribe("/mcu/cmd/#", 0,
-		function(m) print("Subscribed to CMD Topic") end)
+	m:subscribe("smarthome/cmd/#", 0,
+		function(m) print("Subscribed to smarthome/cmd/#") end) --/mcu
 end)
+   
+
 
 
 -- When client disconnects, print a message and list space left on stack
