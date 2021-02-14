fad39a81540d709a6d0cc2f12346c772154d7f95
Gregório Luz <30591064+gregorio1212@users.noreply.github.com>
2021-02-14 23:03:14 +0100

    Some new files

---
+++ Pico-Temperature-1/alarmtempsmart.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+
+void gpio_callback(){
+	gpio_put(5, true);
+}
+
+int main(void){
+	stdio_init_all();
+
+	gpio_init_mask((1<<5) + (1<<25) + (1<<2) + (1<<3) +(1<<4));
+	//OUTPUT Pins
+	//gpio_init(25);
+	gpio_set_dir(25, true);//true equals output
+	//gpio_init(5); //beep output
+	gpio_set_dir(5, true);//true equals output
+
+	//INPUT pins
+	//gpio_init(2); //sound detector
+	//gpio_init(3); //alarm interr
+	//gpio_init(4); //beep interr
+	gpio_pull_down(2); //const DOWN
+	gpio_pull_down(3);
+	gpio_pull_down(4);
+	gpio_set_dir(2, false); //sets as input
+	gpio_set_dir(3, false);
+	gpio_set_dir(4, false);
+	gpio_set_input_enabled(3, true);
+	gpio_set_input_enabled(4, true);
+
+	//Configure ADC
+	adc_init();
+	adc_set_temp_sensor_enabled(true);
+	adc_select_input(4);//4 = inboard temperature sensor
+	uint16_t raw;
+	const float conversion = 3.3f / (1<<12);
+	float voltage;
+	float temperature;
+
+	while(1){
+		sleep_ms(1000);
+		//if beep mqtt is detected, then ring the alarm
+		if(gpio_get(4)){
+			sleep_ms(400);
+			if(gpio_get(4)){
+				gpio_put(5, true);
+				gpio_put(25, false);
+			}
+		}
+		//if alarm off
+		else if(!gpio_get(3)) {
+			gpio_set_input_enabled(2, false); //disable sound detector and ringing alarm
+			gpio_set_irq_enabled_with_callback(2, GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL,false, &gpio_callback);
+			gpio_put(5,false);
+			gpio_put(25, false);
+		}
+		//setting the alarm
+		else if(gpio_get(3)) {
+			sleep_ms(400);
+			if(gpio_get(3)){
+				gpio_put(25,true);
+				gpio_set_input_enabled(2,true);
+				gpio_set_irq_enabled_with_callback(2, GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL,true, &gpio_callback);
+			}
+		}
+		raw = adc_read();
+		voltage = raw*conversion;
+		temperature = 27 - (voltage - 0.706)/0.001721;
+		printf("%f\n",temperature);
+	}
+}
+
---
+++ Pico-Temperature-1/temperature_mqtt.py
@@ -0,0 +1,37 @@
+import serial
+import paho.mqtt.client as mqtt
+
+MQTT_HOST = "89.79.127.216"
+MQTT_PORT = 1883
+
+def on_connect(self, client, userdata,rc):
+	print("Connected with result code "+str(rc)+"\n")
+	print("Subscribed to smarthome/info/temp1")
+
+def on_message_temp(self, client, userdata, msg):
+	print("\n\t* TEMPERATURE UPDATED")
+
+def command_error(self):
+	print("Error: Unknown command")
+
+client = mqtt.Client(client_id="pico-temperature")
+
+client.username_pw_set(username="brazil",password="inpoland2021")
+
+client.on_connect = on_connect
+client.message_callback_add("smarthome/info/temp1", on_message_temp)
+client.will_set("smarthome/info/device2","offline",qos=0, retain=True)
+
+client.connect(MQTT_HOST, MQTT_PORT, 60)
+client.loop_start()
+
+ser = serial.Serial('/dev/serial0', 115200, timeout=1)
+
+client.publish("smarthome/info/device2",payload="online",qos=0,retain=True)
+
+while(1):
+	temp = ser.readline()
+	if(temp!=b''):
+		temp =round(float(temp),1)
+		print("%.1f" % temp)
+		client.publish("smarthome/info/temp1",payload=temp,qos=0,retain=False)
