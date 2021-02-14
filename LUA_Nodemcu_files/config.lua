794d2fc9d7438b38bbec33d23f530b991919a6a3
Gregório <30591064+gregorio1212@users.noreply.github.com>
2021-02-14 21:54:27 +0100

    final version config.lua

--- LUA_Nodemcu_files/config.lua
+++ LUA_Nodemcu_files/config.lua
@@ -1,3 +1,9 @@
+--[[
+config.lua
+
+Global variable configuration file for better portability
+Change for your particular setup. This assumes default Mosquitto config
+--]]
 
 -- Pin Declarations
 PIN_RED = 1
@@ -5,13 +11,16 @@ PIN_GRN = 2
 PIN_BLU = 3
 PIN_BTN = 4
 
+PIN_ALARM = 6 --gpio12
+PIN_BEEP = 5 --gpio 14
+
 -- WiFi
 WIFI_SSID = "Kazik84"
 WIFI_PASS = "KW8417zaq123"
 
 -- MQTT
 MQTT_CLIENTID = "esp-blinkenlite"
-MQTT_HOST = "192.168.1.30"
+MQTT_HOST = "89.79.127.216"--"89.79.127.216" --192.168.1.30
 MQTT_PORT = 1883
 
 -- Confirmation message
